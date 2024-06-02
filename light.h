39e83f026e27d7afe0f2ddb710096bfa50cf57eb
Shiun Robot DIY <masonchen1003@gmail.com>
2024-06-02 08:40:05 +0800

    Update light.h

--- light.h
+++ light.h
@@ -4,14 +4,15 @@
 #include "pxt.h"
 
 // 新增对新 MCU 的定义
-#if defined(MY_NEW_MCU)
+#if defined(RP2040)
 #define BITBANG_SUPPORTED 1
 #endif
 
 // 其他条件编译保持不变
-#if defined(SAMD21) || defined(SAMD51) || defined(STM32F4) || defined(NRF52_SERIES) || defined(MY_NEW_MCU)
+#if defined(SAMD21) || defined(SAMD51) || defined(STM32F4) || defined(NRF52_SERIES) || defined(RP2040)
 #include "neopixel.h"
 #define BITBANG_SUPPORTED 1
 #else
+#include "neopixel.h"
 #define BITBANG_SUPPORTED 1
 #endif
